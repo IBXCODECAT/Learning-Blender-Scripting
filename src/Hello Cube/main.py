489f0fbb512aa5316161585504e0f751c9ae4537
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-12-10 14:50:17 -0600

    Fix Bug

--- src/Hello Cube/main.py
+++ src/Hello Cube/main.py
@@ -10,3 +10,5 @@ bpy.ops.object.mode_set(mode='OBJECT')
 bpy.ops.object.modifier_add(type='SUBSURF')
 bpy.context.object.modifiers["Subdivision"].levels = 2
 bpy.context.object.modifiers["Subdivision"].render_levels = 2
+
+bpy.ops.object.shade_smooth()
