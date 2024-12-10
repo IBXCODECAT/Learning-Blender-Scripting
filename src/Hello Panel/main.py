c759000e406327f72424ee961863ce3e813d0637
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-12-10 15:21:13 -0600

    Inline Comments

--- src/Hello Panel/main.py
+++ src/Hello Panel/main.py
@@ -1,24 +1,37 @@
 import bpy
 
+# Define a custom Panel class
 class TestPanel(bpy.types.Panel):
-    bl_label = "Test Panel"
-    bl_idname = "PT_TestPanel"
+    # The label displayed at the top of the panel
+    bl_label = "Hello Panel!"
+    # A unique identifier for the panel
+    bl_idname = "IBX_Hello_Panel"
+    # Specifies the area where the panel will appear (3D View)
     bl_space_type = 'VIEW_3D'
+    # Specifies the region where the panel will appear (Sidebar)
     bl_region_type = 'UI'
-    bl_category = 'NewTab'
+    # The title of the tab in the sidebar where the panel will be located
+    bl_category = 'Hello Tab!'
 
+    # Function to define the layout of the panel
     def draw(self, context):
-        layout = self.layout
+        layout = self.layout  # Access the panel's layout object
 
-        row = layout.row()
-        row.label(text="Hello Panel!", icon='WORLD_DATA')
+        row = layout.row()  # Create a new row in the panel layout
 
+        # Manipulate the row object with various UI elements
+        row.label(text="Hello World!", icon='WORLD_DATA')
+
+# Register the custom panel class with Blender
 
 def register():
     bpy.utils.register_class(TestPanel)
 
+# Unregister the custom panel class to clean up
+
 def unregister():
     bpy.utils.unregister_class(TestPanel)
 
+# Ensure the script can be executed directly
 if __name__ == "__main__":
     register()
